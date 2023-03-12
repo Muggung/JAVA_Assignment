272f86859c33126c3560d21a371331e9c725fee4
Muggung <mgh0222@naver.com>
2023-03-12 21:32:39 +0900

    학원 과제 (형변환)

--- Assignment/src/Casting/com/bs/practice/CastingPractice.java
+++ Assignment/src/Casting/com/bs/practice/CastingPractice.java
@@ -9,6 +9,7 @@ public class CastingPractice {
 		 * 1. 문자 하나를 입력받는다.
 		 * 2. 입력받은 문자의 유니코드를 출력한다.
 		 */
+		
 		System.out.println("문자 하나를 입력해주세요 : ");
 		char ch = sc.next().charAt(0);
 		System.out.println(ch + " unicode : " + (int) ch);
@@ -21,6 +22,7 @@ public class CastingPractice {
 		 * 2. 입력받은 세 과목의 합과 평균을 출력한다.
 		 * 3. 출력하는 합과 평균은 정수형으로 한다.
 		 */
+		
 		System.out.print("국어 점수를 입력해주세요 : ");
 		double kor = sc.nextDouble();
 		System.out.print("영어 점수를 입력해주세요 : ");
@@ -39,6 +41,7 @@ public class CastingPractice {
 		 * 2. 5개의 변수로 알맞은 사칙연산(+,-,*,/)과 형변환을 이용한다.
 		 * 3. 적힌 값과 같은 값이 나오도록 코드를 작성한다.
 		 */
+		
 		int iNum1 = 10;
 		int iNum2 = 4;
 		
@@ -48,26 +51,26 @@ public class CastingPractice {
 		
 		char ch = 'A';
 		
-		System.out.println(iNum1 - iNum2 * 2);					// 2
-		System.out.println((int)dNum);							// 2
+		System.out.println(iNum1 - iNum2 * 2);						// 2
+		System.out.println((int)dNum);								// 2
 		
-		System.out.println((double)(iNum2 * 2 + (int)dNum));	// 10.0
-		System.out.println((double)iNum1);						// 10.0
+		System.out.println((double)(iNum2 * 2 + (int)dNum));		// 10.0
+		System.out.println((double)iNum1);							// 10.0
 		
-		System.out.println((double)(iNum1 - iNum2 * 2) + 0.5);	// 2.5
-		System.out.println(dNum);								// 2.5
+		System.out.println((double)(iNum1 - iNum2 * 2) + 0.5);		// 2.5
+		System.out.println(dNum);									// 2.5
 		
-		System.out.println((int)fNum);							// 3
-		System.out.println(iNum1 - (int)fNum - iNum2);  		// 3
+		System.out.println((int)fNum);								// 3
+		System.out.println(iNum1 - (int)fNum - iNum2);  			// 3
 		
-		System.out.println(iNum1 / fNum);						// 3.3333333
-		System.out.println(iNum1 / (double)fNum);				// 3.3333333333333335
+		System.out.println(iNum1 / fNum);							// 3.3333333
+		System.out.println(iNum1 / (double)fNum);					// 3.3333333333333335
 		
-		System.out.println(ch);									// 'A'
-		System.out.println((int)ch);							// 65
+		System.out.println(ch);										// 'A'
+		System.out.println((int)ch);								// 65
 		
-		System.out.println((int)ch + iNum1);					// 75
-		System.out.println((char)((int)ch + iNum1));			// 'K'
+		System.out.println((int)ch + iNum1);						// 75
+		System.out.println((char)((int)ch + iNum1));				// 'K'
 	}
 
 }
