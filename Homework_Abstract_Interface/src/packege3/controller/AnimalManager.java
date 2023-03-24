ff4e8a4a45a72850cacacfabf93518a2da9d1c3f
Muggung <mgh0222@naver.com>
2023-03-24 16:18:49 +0900

    학원과제(Abstract&Interface)

---
+++ Homework_Abstract_Interface/src/packege3/controller/AnimalManager.java
@@ -0,0 +1,23 @@
+package packege3.controller;
+
+import package3.model.vo.*;
+
+public class AnimalManager {
+	public static void main(String[] args) {
+		Animal[] animals = new Animal[5];
+		
+		animals[0] = new Cat("야옹이", "고양이", "동제집", "노랑");
+		animals[1] = new Dog("멍멍이", "강아지", 6);
+		animals[2] = new Cat("고등어", "고양이", "동제집", "회색");
+		animals[3] = new Dog("흰둥이", "강아지", 4);
+		animals[4] = new Cat("삼색이", "고양이", "동제집", "삼색");
+		
+		for(Animal a : animals) {
+			if(a instanceof Dog) {
+				((Dog)a).speak();
+			} else if(a instanceof Cat) {
+				((Cat)a).speak();
+			}
+		}
+	}
+}
